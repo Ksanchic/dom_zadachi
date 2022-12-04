d4de6c538d8cf3132fbcc405571868653560af04
ksanchic <ksanchic@mail.ru>
2022-12-04 22:11:24 +0300

    задача8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,25 @@
+﻿{
+int N;
+int i;
+Console.WriteLine ("Введите число N: ");
+N = Convert.ToInt32(Console.ReadLine());
+i = 1;
+
+if (N == 1)
+    {
+   Console.WriteLine("нет четных чисел"); 
+    }
+
+while (i <= N)
+   if (i % 2 == 0)
+   {
+    Console.WriteLine (i);
+    i = i + 1;
+   }
+    else
+   {
+    i = i + 1;
+   }
+   
+Console.ReadLine();
+}
