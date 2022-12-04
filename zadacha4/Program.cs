edad2b49f8b6f3b029b66db7631698521589e8ec
ksanchic <ksanchic@mail.ru>
2022-12-04 22:06:27 +0300

    Задача4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,31 @@
+﻿{
+int numberA;
+int numberB;
+int numberE;
+
+Console.WriteLine ("Введите три числа: ");
+
+numberA = Convert.ToInt32(Console.ReadLine());
+numberB = Convert.ToInt32(Console.ReadLine());
+numberE = Convert.ToInt32(Console.ReadLine());
+
+    if (numberA > numberB)
+        if (numberA > numberE)
+        {
+            Console.WriteLine ("{0} max", numberA);
+        }
+        else
+        {
+            Console.WriteLine ("{0} max", numberE);
+        }
+    else
+        if (numberB > numberE)
+        {
+            Console.WriteLine ("{0} max", numberB);
+        }
+        else
+        {
+            Console.WriteLine ("{0} max", numberE);
+        }
+    Console.ReadLine();
+}
