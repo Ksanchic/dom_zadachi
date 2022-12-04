72b347bed2d7cfc58d9f7f35a1bb35819c46f633
ksanchic <ksanchic@mail.ru>
2022-12-04 22:02:54 +0300

    Задача2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,18 @@
+﻿{
+int numberA;
+int numberB;
+Console.WriteLine ("Введите два числа: ");
+numberA = Convert.ToInt32(Console.ReadLine());
+numberB = Convert.ToInt32(Console.ReadLine());
+    if (numberA > numberB)
+   { 
+    Console.WriteLine ("{0} max", numberA); 
+    Console.WriteLine ("{0} min", numberB);
+   }
+    else
+    {
+        Console.WriteLine ("{0} max", numberB);
+        Console.WriteLine ("{0} min", numberA);
+    }
+    Console.ReadLine();
+}
