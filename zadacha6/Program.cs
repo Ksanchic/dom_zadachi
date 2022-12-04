986f568292c66f68be00e454e9e555ff1084b4d0
ksanchic <ksanchic@mail.ru>
2022-12-04 22:09:01 +0300

    Задача6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,15 @@
+﻿{
+int numberA; 
+Console.WriteLine ("Введите число: ");
+numberA = Convert.ToInt32 (Console.ReadLine());
+
+if (numberA % 2 == 0)
+ {
+    Console.WriteLine("Yes");
+ }
+ else
+ {
+    Console.WriteLine("No");
+ }
+    Console.ReadLine();
+}
